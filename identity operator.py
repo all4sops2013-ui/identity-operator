ad4b1bb26cf2858c23d9a40cdd0ff8efd7d397ee
all4sops2013-ui <all4sops2013@gmail.com>
2026-05-24 13:16:58 +0100

    Add files via upload

---
+++ identity operator.py
@@ -0,0 +1,8 @@
+# is - is an identity operator that is used to check
+
+list1 = [23, 56 , 79, 93, 2]
+list2 = ["Sarah", "Adam", "Mariah", "Leo"]
+list3 = [1, 23.4, 67, 67.67]
+
+list4 = list2
+print(list4 is list2)
